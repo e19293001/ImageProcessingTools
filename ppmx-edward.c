60106519301fc261fbb49f2f652dd8fd961a7c90
e19293001 <e19293001@gmail.com>
2018-06-23 15:21:05 +0800

    finally used bayer 4x4

--- ppmx-edward.c
+++ ppmx-edward.c
@@ -139,6 +139,8 @@ int main(int argc, char **argv)
 
 	handler.arg_flag.rotate_enable = 0;
 	handler.arg_flag.resize_enable = 0;
+    handler.arg_flag.gray_enable = 0;
+
 	handler.filename = NULL;
 
 	for (x = 1; x < argc; x++)
@@ -328,7 +330,6 @@ int putImageToFile(ppm_image_handler *handler)
     {
         unsigned char tmp = 0;
         int z = 0;
-        int a = 0;
         //printBinary(0xAB);
         printf("\n");
         
@@ -1050,35 +1051,117 @@ int mono(ppm_image_handler *handler) // TODO: should return error
         if (handler->imginfo.new_buff[y] == NULL) return -1;
     }
 
-//    int matrix[] = {1, 33,  9, 41, 3,  35, 11, 43, 49, 17, 57, 25, 51, 19, 59, 27, 13, 45, 5, 
-//                37, 15, 47, 7, 39, 61, 29, 53, 21, 63, 31, 55, 23, 4, 36, 12, 44, 2, 34, 10, 42, 52,
-//                20, 60, 28, 50, 18, 58, 26, 16, 48, 8, 40, 14, 46, 6, 38, 64, 32, 56, 24, 62, 30,
-//                54, 22};
+
+/*
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+    float matrix[] = { 0,  8,  2, 10,
+                      12,  4, 14,  6,
+                       3, 11,  1,  9,
+                      15,  7, 13,  5 };
+
+    int k = 4;
+
+    for (x = 0; x < k*k; x++)
+        matrix[x] = (matrix[x] / 16) * 255;
+
+    for (y = 0; y < handler->imginfo.new_height; y++)
+    {
+        for (x = 0; x < handler->imginfo.new_width; x++)
+        {
+            unsigned char oldpixel = (handler->imginfo.buff[y][x].r + handler->imginfo.buff[y][x].g + handler->imginfo.buff[y][x].b) / 3;
+            if (oldpixel >= matrix[x%k*k+y%k])
+                handler->imginfo.new_buff[y][x].r = 0;
+            else
+                handler->imginfo.new_buff[y][x].r = 1;
+        }
+    }
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+/*
+// bayer 8x8 with gamma correction
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+    float matrix[] = {0.1250, 1.0000, 0.1875, 0.8125,
+                    0.6250, 0.3750, 0.6875, 0.4375,
+                    0.2500, 0.8750, 0.0625, 0.9375,
+                    0.7500, 0.5000, 0.5625, 0.3125};
+
+    int k = 4;
+
+    unsigned char gamma_lut[256];
+    float gamma = 2;
+    float gammaCorrection = 1 / gamma;
+
+
+    for (x = 0; x < 256; x++)
+    {
+        //printf("pow(x / 255, gammaCorrection): %0f\n", pow((float)x / 255, gammaCorrection) * 255);
+        gamma_lut[x] = pow((float)x / 255, gammaCorrection) * 255;
+        //gamma_lut[x] = (float) pow(x / 255, gammaCorrection) * 255;
+    }
+
+    //for (x = 0; x < 256; x++)
+    //    printf("gamma_lut[%03d]: %0f\n", x, gamma_lut[x]);
+
+    for (x = 0; x < k*k; x++)
+        matrix[x] = matrix[x] * 255;
+
+    for (y = 0; y < handler->imginfo.new_height; y++)
+    {
+        for (x = 0; x < handler->imginfo.new_width; x++)
+        {
+            unsigned char oldpixel = (handler->imginfo.buff[y][x].r + handler->imginfo.buff[y][x].g + handler->imginfo.buff[y][x].b) / 3;
+            oldpixel = (unsigned char) gamma_lut[oldpixel];
+            if (oldpixel >= matrix[x%k*k+y%k])
+                handler->imginfo.new_buff[y][x].r = 0;
+            else
+                handler->imginfo.new_buff[y][x].r = 1;
+        }
+    }
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+// bayer 4x4  
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+    float matrix[] = {0.1250, 1.0000, 0.1875, 0.8125,
+                    0.6250, 0.3750, 0.6875, 0.4375,
+                    0.2500, 0.8750, 0.0625, 0.9375,
+                    0.7500, 0.5000, 0.5625, 0.3125};
+
+    int k = 4;
+
+    for (x = 0; x < k*k; x++)
+        matrix[x] = matrix[x] * 255;
+
+    for (y = 0; y < handler->imginfo.new_height; y++)
+    {
+        for (x = 0; x < handler->imginfo.new_width; x++)
+        {
+            unsigned char oldpixel = (handler->imginfo.buff[y][x].r + handler->imginfo.buff[y][x].g + handler->imginfo.buff[y][x].b) / 3;
+            if (oldpixel >= matrix[x%k*k+y%k])
+                handler->imginfo.new_buff[y][x].r = 0;
+            else
+                handler->imginfo.new_buff[y][x].r = 1;
+        }
+    }
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+/*
+    float matrix[] = {1, 33,  9, 41, 3,  35, 11, 43, 49, 17, 57, 25, 51, 19, 59, 27, 13, 45, 5, 
+                37, 15, 47, 7, 39, 61, 29, 53, 21, 63, 31, 55, 23, 4, 36, 12, 44, 2, 34, 10, 42, 52,
+                20, 60, 28, 50, 18, 58, 26, 16, 48, 8, 40, 14, 46, 6, 38, 64, 32, 56, 24, 62, 30,
+                54, 22};
 
     int k = 8;
-    int matrix[] = {  0, 32,  8, 40,  2, 34, 10, 42,
-                     48, 16, 56, 24, 50, 18, 58, 26,
-                     12, 44,  4, 36, 14, 46,  6, 38,
-                     60, 28, 52, 20, 62, 30, 54, 22,
-                      3, 35, 11, 43,  1, 33,  9, 41,
-                     51, 19, 59, 27, 49, 17, 57, 25,
-                     15, 47,  7, 39, 13, 45,  5, 37,
-                     63, 31, 55, 23, 61, 29, 53, 21 };
-
-    float smoothKernel[3][3];
-
-    smoothKernel[0][0] = 1.0; smoothKernel[1][0] = 1.0; smoothKernel[2][0] = 1.0;
-    smoothKernel[0][1] = 1.0; smoothKernel[1][1] = -8.0; smoothKernel[2][1] = 1.0;
-    smoothKernel[0][2] = 1.0; smoothKernel[1][2] = 1.0; smoothKernel[2][2] = 1.0;
-
-//    int matrix[] = {34, 48, 40, 32, 29, 15, 23, 31, // dot diffusion
-//                    42, 58, 56, 53, 21,  5,  7, 10,
-//                    50, 62, 61, 45, 13,  1,  2, 18,
-//                    38, 46, 54, 37, 25, 17,  9, 26,
-//                    28, 14, 22, 30, 35, 49, 41, 33,
-//                    20,  4,  6, 11, 43, 59, 57, 52,
-//                    12,  0,  3, 19, 51, 63, 60, 44,
-//                    24, 16,  8, 27, 39, 47, 55, 36};
+//    int matrix[] = {  0, 32,  8, 40,  2, 34, 10, 42,
+//                     48, 16, 56, 24, 50, 18, 58, 26,
+//                     12, 44,  4, 36, 14, 46,  6, 38,
+//                     60, 28, 52, 20, 62, 30, 54, 22,
+//                      3, 35, 11, 43,  1, 33,  9, 41,
+//                     51, 19, 59, 27, 49, 17, 57, 25,
+//                     15, 47,  7, 39, 13, 45,  5, 37,
+//                     63, 31, 55, 23, 61, 29, 53, 21 };
 
     for (x = 0; x < 64; x++)
         matrix[x] = matrix[x] * 4 - 1;
@@ -1087,73 +1170,16 @@ int mono(ppm_image_handler *handler) // TODO: should return error
     {
         for (x = 0; x < handler->imginfo.new_width; x++)
         {
-
-            // get the average of all neighbors
-            int alpha = 0.9;
-            int zz = k >> 1;
-            handler->imginfo.new_buff[y][x].r = (unsigned char)((handler->imginfo.buff[y][x].r + handler->imginfo.buff[y][x].g + handler->imginfo.buff[y][x].b) / 3);
-            //if (y >= zz && x >= zz && y < handler->imginfo.new_height - zz - 1&& x < handler->imginfo.new_width - zz - 1)
-            //{
-            //    int yy;
-            //    int xx;
-            //    int sum_r = 0;
-            //    for (yy = -zz; yy <= zz; ++yy) {
-            //        for (xx = -zz; xx <= zz; ++xx) {
-            //            sum_r += ((handler->imginfo.buff[y+yy][x+xx].r + handler->imginfo.buff[y+yy][x+xx].g + handler->imginfo.buff[y+yy][x+xx].b)/3) * smoothKernel[yy+zz][xx+zz];
-            //        }
-            //    }
-            //    sum_r = (handler->imginfo.new_buff[y][x].r/3) - 0.2 * sum_r;
-            //
-            //    if (sum_r < 0.0f) sum_r = 0.0f;
-            //
-            //    if (sum_r >= 256.0f) sum_r = 255.0f;
-            //
-            //    handler->imginfo.new_buff[y][x].r = (char)sum_r;
-            //    //printf("sum_r: %0d\n", sum_r);
-            //}
-            //
-            ////unsigned char oldpixel = handler->imginfo.buff[y][x].r;
-            //if (handler->imginfo.new_buff[y][x].r >= matrix[x%k*k+y%k])
-            //    handler->imginfo.new_buff[y][x].r = 0;
-            //else
-            //    handler->imginfo.new_buff[y][x].r = 1;
-
-            //if (y >= zz && x >= zz && y < handler->imginfo.new_height - zz - 1 && x < handler->imginfo.new_width - zz - 1)
-            //{
-            //    int yy;
-            //    int xx;
-            //    int sum_r = 0;
-            //    int sum_g = 0;
-            //    int sum_b = 0;
-            //    for (yy = -zz; yy <= zz; ++yy) {
-            //        for (xx = -zz; xx <= zz; ++xx) {
-            //            sum_r += handler->imginfo.new_buff[y+yy][x+xx].r;
-            //        }
-            //    }
-            //    handler->imginfo.new_buff[y][x].r = (handler->imginfo.new_buff[y+yy][x+xx].r - alpha * (sum_r/9)) / (1 - alpha);
-            //}
+            unsigned char oldpixel = (handler->imginfo.buff[y][x].r + handler->imginfo.buff[y][x].g + handler->imginfo.buff[y][x].b) / 3;
+            if (oldpixel >= matrix[x%k*k+y%k])
+                handler->imginfo.new_buff[y][x].r = 0;
+            else
+                handler->imginfo.new_buff[y][x].r = 1;
 
 
         }
     }
-
-//    int matrix[4][4] = {{1, 9, 3, 11} , {13, 5, 15, 7} , {4, 12, 2, 10} , {16, 8, 14, 6}};
-//    for (y = 0; y < handler->imginfo.new_height; y++)
-//    {
-//        for (x = 0; x < handler->imginfo.new_width; x++)
-//        {
-//            float mratio = 1.0 / 12;
-//            float mfactor = 255.0 / 5;
-//
-//            unsigned char oldpixel = (unsigned char)((handler->imginfo.buff[y][x].r + handler->imginfo.buff[y][x].g + handler->imginfo.buff[y][x].b) / 3);
-//            unsigned char value = oldpixel + (mratio * matrix[y%4][x%4] * mfactor);
-//            if (value < 128)
-//                handler->imginfo.new_buff[y][x].r = 1;
-//            else
-//                handler->imginfo.new_buff[y][x].r = 0;
-//        }
-//    }
-
+*/
     return 0;
 }
 
